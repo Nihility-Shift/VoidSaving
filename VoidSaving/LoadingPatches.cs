7fd61b26d5675f2f811155422372342a823290fe
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-29 22:33:16 -0800

    Added GetSessionSaveGameData

---
+++ VoidSaving/LoadingPatches.cs
@@ -0,0 +1,38 @@
+﻿using Gameplay.Quests;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Services.CloudSave;
+
+namespace VoidSaving
+{
+    [HarmonyPatch]
+    internal class LoadingPatches
+    {
+        [HarmonyPatch(typeof(EndlessQuestGenerator), "CreateEndlessQuest"), HarmonyPostfix]
+        static void QuestLoadPatch(Quest __result)
+        {
+            EndlessQuest quest = __result as EndlessQuest;
+            if (!SaveHandler.LoadSavedData || quest == null) return;
+            {
+
+            }
+
+            SaveGameData saveData = SaveHandler.ActiveData;
+
+            quest.QuestParameters.Seed = saveData.seed;
+            quest.JumpCounter = saveData.JumpCounter;
+            quest.InterdictionCounter = saveData.InterdictionCounter;
+            quest.context.Random = saveData.random;
+        }
+
+        [HarmonyPatch(typeof(GameSession), "LoadGameSessionNetworkedAssets"), HarmonyPrefix]
+        static void ShipLoadPatch(GameSession __instance)
+        {
+            __instance.ToLoadShipData = ShipLoadout.FromJObject(SaveHandler.ActiveData.ShipLoadout);
+        }
+    }
+}
--- VoidSaving/SaveHandler.cs
+++ VoidSaving/SaveHandler.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CG.Game;
+using CG.Ship.Repair;
+using CG.Space;
+using Gameplay.Quests;
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VoidSaving
 {
@@ -12,6 +12,8 @@ namespace VoidSaving
         public static SaveGameData ActiveData { get; internal set; }
 
         public static bool StartedAsHost { get; internal set; }
+
+        internal static bool LoadSavedData = false;
 
         public static string SavesLocation
         {
@@ -25,9 +27,48 @@ namespace VoidSaving
             }
         }
 
+        internal static SaveGameData GetSessionSaveGameData()
+        {
+            SaveGameData saveGameData = new SaveGameData();
+            GameSession session = GameSessionManager.Instance.activeGameSession;
+            AbstractPlayerControlledShip playerShip = ClientGame.Current.PlayerShip;
+
+            //Ship data
+            saveGameData.Alloy = GameSessionSuppliesManager.Instance.AlloyAmount;
+            saveGameData.Biomass = GameSessionSuppliesManager.Instance.BiomassAmount;
+            saveGameData.ShipHealth = ClientGame.Current.PlayerShip.HitPoints;
+
+            saveGameData.ShipLoadout = new ShipLoadout(PlayerShipManager.Instance.ActivePlayerShip).AsJObject();
+            saveGameData.Relics = Helpers.RelicGUIDsFromShip(playerShip);
+            saveGameData.UnlockedBPs = Helpers.UnlockedBPGUIDsFromShip(playerShip);
+
+            HullDamageController HDC = ClientGame.Current.PlayerShip.GetComponent<HullDamageController>();
+            saveGameData.RepairableShipHealth = HDC.State.repairableHp;
+            saveGameData.Breaches = Helpers.BreachesAsConditionsArray(HDC.breaches);
+
+            //Quest data
+            EndlessQuest activeQuest = session.ActiveQuest as EndlessQuest;
+
+            saveGameData.seed = activeQuest.QuestParameters.Seed;
+            saveGameData.JumpCounter = activeQuest.JumpCounter;
+            saveGameData.InterdictionCounter = activeQuest.InterdictionCounter;
+            saveGameData.random = activeQuest.Context.Random;
+
+
+            return saveGameData;
+        }
+
         public static bool LoadSave(string SavePath)
         {
             SaveGameData data = new SaveGameData();
+
+            using (FileStream fileStream = File.OpenRead(SavePath))
+            {
+                using (BinaryReader reader = new BinaryReader(fileStream))
+                {
+
+                }
+            }
 
             ActiveData = data;
             return false;
