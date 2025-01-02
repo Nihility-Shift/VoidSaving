1d5c28887102c2d6569cd29dd4f542db834f4c98
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-01 23:00:18 -0800

    Added extension classes

---
+++ VoidSaving/RandomSerializer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidSaving
+{
+    //Stolen from zORg Alex, https://stackoverflow.com/a/67604192
+    public static class RandomSerializer
+    {
+        //* Used for Getting and setting System.Random state *//
+        private static System.Reflection.FieldInfo[] randomFields;
+        public static System.Reflection.FieldInfo[] RandomFields
+        {
+            get
+            {
+                if (randomFields == null)
+                {
+                    randomFields = new System.Reflection.FieldInfo[3];
+                    var t = typeof(System.Random);
+                    randomFields[0] = t.GetField("_seedArray", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    randomFields[1] = t.GetField("_inext", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    randomFields[2] = t.GetField("_inextp", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                }
+                return randomFields;
+            }
+        }
+        /// <summary>
+        /// Gets <see cref="System.Random"/> current state array and indexes with Reflection.
+        /// </summary>
+        /// <param name="rand"></param>
+        /// <returns></returns>
+        public static int[] GetSeedArray(this System.Random rand)
+        {
+            var state = new int[58];
+            ((int[])RandomFields[0].GetValue(rand)).CopyTo(state, 0);
+            state[56] = (int)RandomFields[1].GetValue(rand);
+            state[57] = (int)RandomFields[2].GetValue(rand);
+            return state;
+        }
+
+        /// <summary>
+        /// Restores saved <see cref="System.Random"/> state and indexes with Reflection. Use with caution.
+        /// </summary>
+        /// <param name="rand"></param>
+        /// <param name="seedArray"></param>
+        public static void SetSeedArray(this System.Random rand, int[] seedArray)
+        {
+            if (seedArray.Length != 56 + 2) return;
+
+            Array.Copy(seedArray, ((int[])RandomFields[0].GetValue(rand)), 56);
+            RandomFields[1].SetValue(rand, seedArray[56]);
+            RandomFields[2].SetValue(rand, seedArray[57]);
+        }
+
+    }
+}
---
+++ VoidSaving/ReadWriteExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace VoidSaving
+{
+    public static class ReadWriteExtensions
+    {
+        public static void Write(this BinaryWriter Writer, GUIDUnion union)
+        {
+            foreach (int item in union.AsIntArray())
+            {
+                Writer.Write(item);
+            }
+        }
+
+        public static GUIDUnion ReadGUIDUnion(this BinaryReader Reader)
+        {
+            int[] GUIDArray = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                GUIDArray[i] = Reader.ReadInt32();
+            }
+            return new GUIDUnion(GUIDArray);
+        }
+
+
+        public static void Write(this BinaryWriter Writer, GUIDUnion[] unions)
+        {
+            Writer.Write(unions.Length);
+            foreach(GUIDUnion union in unions)
+            {
+                Writer.Write(union);
+            }
+        }
+
+        public static GUIDUnion[] ReadGUIDUnionArray(this BinaryReader Reader)
+        {
+            int count = Reader.ReadInt32();
+            GUIDUnion[] unions = new GUIDUnion[count];
+            for (int i = 0; i < count; i++)
+            {
+                unions[i] = Reader.ReadGUIDUnion();
+            }
+            return unions;
+        }
+
+
+        public static void Write(this BinaryWriter Writer, int[] ints)
+        {
+            Writer.Write(ints.Length);
+            foreach (int inty in ints)
+            {
+                Writer.Write(inty);
+            }
+        }
+
+        public static int[] ReadInt32Array(this BinaryReader Reader)
+        {
+            int count = Reader.ReadInt32();
+            int[] unions = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                unions[i] = Reader.ReadInt32();
+            }
+            return unions;
+        }
+
+
+        public static void Write(this BinaryWriter Writer, JObject jobject)
+        {
+            //Converts JObject to string, then writes string, converting to byte[]. It's more preferable to convert the JObject directly to a byte[], but I'm not sure if that's possible
+            Writer.Write(jobject.ToString(Newtonsoft.Json.Formatting.None));
+        }
+
+        public static JObject ReadJObject(this BinaryReader Reader)
+        {
+            return JObject.Parse(Reader.ReadString());
+        }
+
+
+        public static void Write(this BinaryWriter Writer, Random random)
+        {
+            Writer.Write(random.GetSeedArray());
+        }
+
+        public static Random ReadRandom(this BinaryReader reader)
+        {
+            Random returnValue = new();
+            returnValue.SetSeedArray(reader.ReadInt32Array());
+            return returnValue;
+        }
+    }
+}
