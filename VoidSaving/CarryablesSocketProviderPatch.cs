221f5527e2fea8ba1c178b902c25cf6754f34493
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-02 17:54:15 -0800

    Added CarryablesSocketProviderPatch.cs

---
+++ VoidSaving/CarryablesSocketProviderPatch.cs
@@ -0,0 +1,15 @@
+﻿using CG.Ship.Modules;
+using HarmonyLib;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(CarryablesSocketProvider), "Awake")]
+    internal class CarryablesSocketProviderPatch
+    {
+        static void Prefix(CarryablesSocketProvider __instance)
+        {
+            //Never called befure, but required for socket provider to be aware of sockets. This is called to help the existing ship save system detect installed mods/batteries.
+            __instance.FindSockets();
+        }
+    }
+}
