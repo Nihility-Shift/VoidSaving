3db04c67f2935d8723917ea23abc2ec6cb5abfbe
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-15 01:37:53 -0800

    Added patch for upgrader load

---
+++ VoidSaving/UpgraderShelfLoadPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace VoidSaving
+{
+    //Upgraders being placed in shelves on ship load break here. Stop original code from running if attempting to run (no calls needed from vanilla at time of writing)
+    [HarmonyPatch(typeof(ModuleUpgraderEffects), "StartBeingCarried")]
+    internal class UpgraderShelfLoadPatch
+    {
+        static bool Prefix()
+        {
+            if (SaveHandler.LoadSavedData)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
