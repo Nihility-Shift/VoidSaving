ea47114b096078d5484c68216df51d0493e139c3
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-25 22:23:11 -0800

    Fixed loading speed issue with normally harmless network delay.

---
+++ VoidSaving/SessonManagerPropertiesFix.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(GameSessionSectorManager), "OnRoomPropertiesUpdate")]
+    internal class SessonManagerPropertiesFix
+    {
+        static bool Prefix()
+        {
+            //If master client, value should have already been processed.
+            return !PhotonNetwork.IsMasterClient;
+        }
+    }
+}
