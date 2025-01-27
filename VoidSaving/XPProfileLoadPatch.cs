691bf04f653239cdaac447b2a0ed8f7dcb43871b
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-27 02:13:58 -0800

    Fixed XP issue which will be found by testers.

---
+++ VoidSaving/XPProfileLoadPatch.cs
@@ -0,0 +1,14 @@
+﻿using CG.Cloud;
+using HarmonyLib;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(CloudPlayerProfileDataSync), "ClaimPendingXp")]
+    internal class XPProfileLoadPatch
+    {
+        static void Postfix(CloudPlayerProfileDataSync __instance)
+        {
+            __instance.UploadProfile();
+        }
+    }
+}
