5548b4a3c8d253c374f6bbd757f1a096e6a9d69f
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-29 13:28:07 -0800

    IronMan now deleting save on ship death.

---
+++ VoidSaving/IronManDeleteOnDeathPatch.cs
@@ -0,0 +1,15 @@
+﻿using CG.Space;
+using HarmonyLib;
+
+namespace VoidSaving
+{
+    //Handle here to avoid exploit-based save loss.
+    [HarmonyPatch(typeof(AbstractPlayerControlledShip), "Kill")]
+    internal class IronManDeleteOnDeathPatch
+    {
+        static void Postfix()
+        {
+            SaveHandler.DeleteSaveFile(SaveHandler.LastSaveName);
+        }
+    }
+}
