b4e33fc8865e482a3462e22f04f2e005bd8ad13b
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-29 19:10:09 -0800

    Fixed issue caused by exiting to main menu while loading a save.

---
+++ VoidSaving/LeaveGameCancelLoadPatch.cs
@@ -0,0 +1,14 @@
+﻿using CG.GameLoopStateMachine.GameStates;
+using HarmonyLib;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(GSQuitFromMenu), "OnEnter")]
+    internal class LeaveGameCancelLoadPatch
+    {
+        static void Postfix()
+        {
+            SaveHandler.CancelOrFinalzeLoad();
+        }
+    }
+}
