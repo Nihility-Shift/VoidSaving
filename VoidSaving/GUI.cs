c18c7feef92b8d01aa508711400718b3aaafb010
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-31 03:52:44 -0800

    Added more spacing to Iron Man toggles.

--- VoidSaving/GUI.cs
+++ VoidSaving/GUI.cs
@@ -42,7 +42,7 @@ namespace VoidSaving
             {
                 BeginHorizontal();
                 SaveFilePeekData data = KVP.Value;
-                if (GUITools.DrawButtonSelected($"{KVP.Key}{(data.IronMan ? " (IronMan)" : string.Empty)}{(data.ProgressDisabled ? " (Progress Disabled)" : string.Empty)} - {data.writeTime} | {data.ShipName}, {data.JumpCounter} Jumps. {(int)data.TimePlayed.TotalHours}:{data.TimePlayed.Minutes} Played", SaveName == KVP.Key))
+                if (GUITools.DrawButtonSelected($"{KVP.Key}{(data.IronMan ? " (IronMan)" : string.Empty)}{(data.ProgressDisabled ? " (Progress Disabled)" : string.Empty)} - {data.writeTime} | {data.ShipName}, {data.JumpCounter} Jumps. {(int)data.TimePlayed.TotalHours}:{data.TimePlayed.Minutes.ToString("00")} Played", SaveName == KVP.Key))
                 {
                     SaveName = KVP.Key;
                 }
@@ -75,6 +75,7 @@ namespace VoidSaving
         {
             BeginHorizontal();
             GUITools.DrawCheckbox("Auto Saves Enabled", ref Config.AutoSavingEnabled);
+            FlexibleSpace(); FlexibleSpace();
             GUITools.DrawTextField("Auto Save Limit", ref Config.AutoSaveLimit);
             EndHorizontal();
         }
