f0785f8c4bc5acf846b83e6ebb97ff4e8e48d839
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-01-31 00:54:52 -0800

    Now notifying host of iron man on start.

---
+++ VoidSaving/IronManNotifiyPatch.cs
@@ -0,0 +1,23 @@
+﻿using CG.GameLoopStateMachine;
+using CG.GameLoopStateMachine.GameStates;
+using HarmonyLib;
+using VoidManager.Utilities;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(GSIngame), "OnEnter")]
+    internal class IronManNotifiyPatch
+    {
+        static void Postfix(IState previous)
+        {
+            if (previous is GSSpawn && GameSessionManager.InHub && SaveHandler.StartedAsHost)
+            {
+                SaveHandler.LatestData = null;
+                if (SaveHandler.IsIronManMode)
+                {
+                    Messaging.Notification("Iron Man for the next session is ON");
+                }
+            }
+        }
+    }
+}
