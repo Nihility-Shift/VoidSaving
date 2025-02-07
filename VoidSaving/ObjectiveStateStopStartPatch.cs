ecbb7cdd1d30bf63245e7d04350f7f76c9974085
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-06 23:12:36 -0800

    Added patch for load-in sector state issue

---
+++ VoidSaving/ObjectiveStateStopStartPatch.cs
@@ -0,0 +1,17 @@
+﻿using CG.Game.Scenarios;
+using Gameplay.Quests;
+using HarmonyLib;
+
+namespace VoidSaving
+{
+    //On load state is assigned to started, however if the mission is already failed there's no need for this.
+    [HarmonyPatch(typeof(QuestManager), "SetQuestObjectiveState")]
+    internal class ObjectiveStateStopStartPatch
+    {
+        static bool Prefix(Objective objective, ObjectiveState state)
+        {
+            if (objective.State is ObjectiveState.Completed or ObjectiveState.Failed) return false;
+            return true;
+        }
+    }
+}
