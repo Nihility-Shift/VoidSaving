2478585d98f9a829e5dd8033ddeb0e2565ec4492
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-06 23:14:07 -0800

    Fixed map not realizing it's still game start

---
+++ VoidSaving/AstralMapStartPatch.cs
@@ -0,0 +1,16 @@
+﻿using Gameplay.Quests;
+using HarmonyLib;
+using UI.AstralMap;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(AstralMapController), "CheckForGameStart")]
+    internal class AstralMapStartPatch
+    {
+        static bool Prefix(AstralMapController __instance, EndlessQuest eq)
+        {
+            __instance._gameStart = __instance._lastSector == null || eq.StartSector == __instance._lastSector.Sector;
+            return false;
+        }
+    }
+}
