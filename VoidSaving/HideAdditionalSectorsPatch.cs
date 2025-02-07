99199bb05e157ad2dc86340c025ca03cce9f8351
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-06 23:11:07 -0800

    Broken patch to stop loading additional sectors in all sectors calls

---
+++ VoidSaving/HideAdditionalSectorsPatch.cs
@@ -0,0 +1,18 @@
+﻿using Gameplay.Quests;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace VoidSaving
+{
+    [HarmonyPatch(typeof(GameSessionSection), "AllAvailableSectors", MethodType.Getter)]
+    internal class HideAdditionalSectorsPatch
+    {
+        //Prefix replacement somehow created an infinite load loop.
+        static void Postfix(GameSessionSection __instance, ref List<GameSessionSector> __result)
+        {
+            List<GameSessionSector> list = new List<GameSessionSector>(__instance.ObjectiveSectors);
+            if (__instance.InterdictionSector != null) list.Add(__instance.InterdictionSector);
+            __result = list;
+        }
+    }
+}
