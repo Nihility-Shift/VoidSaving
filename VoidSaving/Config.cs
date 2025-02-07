cd7e2d8e7a9929e3bafcd478e46226b01db2e475
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-06 23:57:23 -0800

    Added interdiction timer padding

--- VoidSaving/Config.cs
+++ VoidSaving/Config.cs
@@ -8,9 +8,10 @@ namespace VoidSaving
         {
             SavesLocation = configFile.Bind("Settings", "SavesLocation", string.Empty);
             LastSave = configFile.Bind("Settings", "LastSave", string.Empty);
-            AutoSavingEnabled = configFile.Bind("Settings", "AutoSaving", true);
-            AutoSaveLimit = configFile.Bind("Settings", "AutoSaveLimit", 10);
-            DefaultIronMan = configFile.Bind("Settings", "DefaultIronManMode", true);
+            AutoSavingEnabled = configFile.Bind("Settings", "AutoSaving", true, "Auto Saving Enabled/Disabled");
+            AutoSaveLimit = configFile.Bind("Settings", "AutoSaveLimit", 10, "How many auto save IDs to loop through");
+            DefaultIronMan = configFile.Bind("Settings", "DefaultIronManMode", true, "Default state for iron man setting.");
+            ExtraMSUntilInterdiction = configFile.Bind("Settings", "ExtraMSUntilInterdiction", 10000, "Time in MS added to interdictiosn during load.");
             LastAutoSave = configFile.Bind("Data", "LastAutoSave", 0);
         }
 
@@ -20,5 +21,6 @@ namespace VoidSaving
         internal static ConfigEntry<int> AutoSaveLimit;
         internal static ConfigEntry<int> LastAutoSave;
         internal static ConfigEntry<bool> DefaultIronMan;
+        internal static ConfigEntry<int> ExtraMSUntilInterdiction;
     }
 }
---
+++ VoidSaving/LoadInInterdictionTimerPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+namespace VoidSaving
+{
+    [HarmonyPatch]
+    internal class LoadInInterdictionTimerPatches
+    {
+        [HarmonyPatch(typeof(VoidJumpTravellingStable), "OnEnter"), HarmonyPostfix]
+        static void VoidJumpStableInterdictionTimerPatch(VoidJumpTravellingStable __instance)
+        {
+            if (SaveHandler.LoadSavedData)
+            {
+                __instance.DurationUntilUnstable += Config.ExtraMSUntilInterdiction.Value;
+            }
+        }
+
+        [HarmonyPatch(typeof(VoidJumpTravellingUnstable), "OnEnter"), HarmonyPostfix]
+        static void VoidJumpUnstableInterdictionTimerPatch(VoidJumpTravellingUnstable __instance)
+        {
+            if (SaveHandler.LoadSavedData)
+            {
+                __instance.DurationUntilInterdiction += Config.ExtraMSUntilInterdiction.Value;
+            }
+        }
+    }
+}
