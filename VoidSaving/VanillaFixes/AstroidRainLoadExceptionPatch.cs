8b6078fa2b371c065a6ae9679fc0ddb209585015
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-26 01:28:04 -0800

    Fixed asteroid rain class breaking on load

---
+++ VoidSaving/VanillaFixes/AstroidRainLoadExceptionPatch.cs
@@ -0,0 +1,15 @@
+﻿using CG.Client.Quests.SectorTwists;
+using HarmonyLib;
+
+namespace VoidSaving.VanillaFixes
+{
+    //AstroidRain tries to read the player ship, which doesn't exist during early load. Prevent execution if loading.
+    [HarmonyPatch(typeof(AsteroidRain), "SetRandomSpawnDirection")]
+    internal class AstroidRainLoadExceptionPatch
+    {
+        static bool Prefix()
+        {
+            return !SaveHandler.LoadSavedData;
+        }
+    }
+}
