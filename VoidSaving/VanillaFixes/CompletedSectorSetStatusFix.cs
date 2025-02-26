a150054da812c4e9b6964ee34dd6e3e15201e39c
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-25 23:53:29 -0800

    Fixed issue with mission status not updating properly.

---
+++ VoidSaving/VanillaFixes/CompletedSectorSetStatusFix.cs
@@ -0,0 +1,43 @@
+﻿using CG.Game.Scenarios;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using static VoidManager.Utilities.HarmonyHelpers;
+
+namespace VoidSaving.VanillaFixes
+{
+    //When 'CompleteSector' is called, the mission status is not properly updated.
+    [HarmonyPatch(typeof(EndlessQuestManager), "CompleteSector")]
+    internal class CompletedSectorSetStatusFix
+    {
+        static void PatchMethod(EndlessQuestManager instance, byte status)
+        {
+            ObjectiveState state = status == 4 ? ObjectiveState.Completed : ObjectiveState.Failed;
+
+            GameSessionSector sector = instance.endlessQuest.Context.CompletedSectors.Last();
+            if (sector.SectorObjective != null)
+            {
+                sector.SectorObjective.Objective.State = state;
+            }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            CodeInstruction[] targetSequence = new CodeInstruction[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(EndlessQuestManager), "RefreshSectorVisibility"))
+            };
+
+            CodeInstruction[] patchSequence = new CodeInstruction[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CompletedSectorSetStatusFix), "PatchMethod"))
+            };
+
+            return PatchBySequence(instructions, targetSequence, patchSequence, PatchMode.BEFORE, CheckMode.NONNULL);
+        }
+    }
+}
