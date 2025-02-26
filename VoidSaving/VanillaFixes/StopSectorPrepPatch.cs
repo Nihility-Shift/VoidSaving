47d669613db66cef2ba0afa31ce9c07f91161687
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-02-26 01:50:43 -0800

    Fixed Twists throwing exception during early load.

---
+++ VoidSaving/VanillaFixes/StopSectorPrepPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace VoidSaving.VanillaFixes
+{
+    //Some sector loading is not needed or breaks, so stop early.
+    [HarmonyPatch(typeof(GameSessionSector), "CreateTwist")]
+    internal class StopSectorPrepPatch
+    {
+        static bool Prefix()
+        {
+            return !SaveHandler.LoadSavedData;
+        }
+    }
+}
