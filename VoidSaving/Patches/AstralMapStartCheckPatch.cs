6a23c78b6ec71cf51b9e7a37403f8b241c1de7c6
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2025-03-02 00:26:57 -0800

    Fixed loading into starting sector

---
+++ VoidSaving/Patches/AstralMapStartCheckPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UI.AstralMap;
+
+namespace VoidSaving.Patches
+{
+    //While loading a save created immediately after the starting sector, the map falsly detects the game as not being immediately after the start section.
+    [HarmonyPatch(typeof(AstralMapController), "CheckForGameStart")]
+    class AstralMapStartCheckPatch
+    {
+        static bool Prefix(AstralMapController __instance)
+        {
+            if (GameSessionManager.ActiveSession.ActiveSectorId == 0)
+            {
+                __instance._gameStart = true;
+                return false;
+            }
+            return true;
+        }
+    }
+}
